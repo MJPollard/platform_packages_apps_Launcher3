c73429f57f75a51751f117550ca436a4fb8cdf84
Raman Tenneti <rtenneti@google.com>
2020-07-31 13:59:41 -0700

    AOSP/Launcher3 - Update language to comply with Android's inclusive language guidance.
    Fixed checkstyle errors.
    
    See https://source.android.com/setup/contribute/respectful-code for reference
    
    BUG=161896447
    
    Test: m and Launcher3Tests
    $ find out -name "Launcher*.apk" -print -exec ls -l {} \;
    -rw-r----- 1 rtenneti primarygroup 947525 Jul 31 11:26 out/target/product/bonito/testcases/Launcher3Tests/arm64/Launcher3Tests.apk
    
    -rw-r----- 1 rtenneti primarygroup 9249956 Jul 31 11:22 out/target/product/bonito/system_ext/priv-app/Launcher3QuickStep/Launcher3QuickStep.apk
    
    Change-Id: Icc0da24ed1e79aaddd28e0194fa52f96c6c3914f

--- src/com/android/launcher3/dragndrop/PinShortcutRequestActivityInfo.java
+++ src/com/android/launcher3/dragndrop/PinShortcutRequestActivityInfo.java
@@ -29,14 +29,14 @@ import android.os.Build;
 import android.os.Process;
 
 import com.android.launcher3.FastBitmapDrawable;
-import com.android.launcher3.WorkspaceItemInfo;
-import com.android.launcher3.icons.IconCache;
 import com.android.launcher3.LauncherAnimUtils;
 import com.android.launcher3.LauncherAppState;
 import com.android.launcher3.LauncherSettings;
 import com.android.launcher3.R;
+import com.android.launcher3.WorkspaceItemInfo;
 import com.android.launcher3.compat.LauncherAppsCompatVO;
 import com.android.launcher3.compat.ShortcutConfigActivityInfo;
+import com.android.launcher3.icons.IconCache;
 
 /**
  * Extension of ShortcutConfigActivityInfo to be used in the confirmation prompt for pin item
@@ -47,14 +47,14 @@ class PinShortcutRequestActivityInfo extends ShortcutConfigActivityInfo {
 
     // Class name used in the target component, such that it will never represent an
     // actual existing class.
-    private static final String DUMMY_COMPONENT_CLASS = "pinned-shortcut";
+    private static final String STUB_COMPONENT_CLASS = "pinned-shortcut";
 
     private final PinItemRequest mRequest;
     private final ShortcutInfo mInfo;
     private final Context mContext;
 
     public PinShortcutRequestActivityInfo(PinItemRequest request, Context context) {
-        super(new ComponentName(request.getShortcutInfo().getPackage(), DUMMY_COMPONENT_CLASS),
+        super(new ComponentName(request.getShortcutInfo().getPackage(), STUB_COMPONENT_CLASS),
                 request.getShortcutInfo().getUserHandle());
         mRequest = request;
         mInfo = request.getShortcutInfo();
