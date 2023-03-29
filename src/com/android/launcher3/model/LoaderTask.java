22cc7aa8f74664e84c6a3e4b40380c916a8c6e87
Oliver Scott <oscott@osomprivacy.com>
2023-03-29 17:27:16 -0400

    Only fail loading apps if the current user's activity list is empty
    Change-Id: Ia225c0b48e9540d51f8216cdee219f3c2f88475b

--- src/com/android/launcher3/model/LoaderTask.java
+++ src/com/android/launcher3/model/LoaderTask.java
@@ -961,10 +961,12 @@ public class LoaderTask implements Runnable {
         for (UserHandle user : profiles) {
             // Query for the set of apps
             final List<LauncherActivityInfo> apps = mLauncherApps.getActivityList(null, user);
-            // Fail if we don't have any apps
-            // TODO: Fix this. Only fail for the current user.
             if (apps == null || apps.isEmpty()) {
-                return allActivityList;
+                if (Process.myUserHandle() == user) {
+                    return allActivityList;
+                } else {
+                    continue;
+                }
             }
             boolean quietMode = mUserManagerState.isUserQuiet(user);
             // Create the ApplicationInfos
